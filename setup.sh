374a559206304b69ec7e77d1e58478ed431fba61
PaisonDev <36568869+PaisonDev@users.noreply.github.com>
2018-02-17 08:58:07 -0800

    Updated setup.sh (No Failure)

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ if [ $(echo $DISTRO | grep -i "CentOs" | wc -l) -eq 1 ]; then
         yum install python gcc screen -y
         sudo iptables -I INPUT -p tcp --dport 80 -j ACCEPT
         sudo service iptables save
-        sudo yum install php php-mysql php-devel php-gd php-pecl-memcache php-pspell php-snmp php-xmlrpc php-xml
+        sudo yum install php php-mysql php-devel php-gd php-pecl-memcache php-pspell php-snmp php-xmlrpc php-xml -y
         sudo service httpd restart
         yum install python-setuptools -y
         curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
@@ -19,12 +19,19 @@ elif [ $(echo $DISTRO | grep -i "Debian" | wc -l) -eq 1 ]; then
         apt-get install apache2 -y
         apt-get install python gcc screen -y
         sudo service apache2 restart
+        apt-get install python-setuptools -y
+        apt-get install python-pip -y
+        pip install ipgetter
 
 elif [ $(echo $DISTRO | grep -i "Ubuntu" | wc -l) -eq 1 ]; then
         apt-get install php libapache2-mod-php php-mcrypt php-mysql -y
         apt-get install apache2 -y
         apt-get install python gcc screen -y
         sudo service apache2 restart
+        apt-get install python-setuptools -y
+        apt-get install python-pip -y
+        pip install ipgetter
+        
 else
         echo "Unknown OS"
 fi
